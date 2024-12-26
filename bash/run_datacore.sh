842768398bc2c5000ad0ee7d3c5d668aea0bc457
timur <timurzavalov@gmail.com>
2024-12-26 12:06:23 +0300

    add: added new table sales_plan_pivot_table

--- bash/run_datacore.sh
+++ bash/run_datacore.sh
@@ -6,5 +6,6 @@ do
 	${XL_IDP_ROOT_DATACORE}/bash/border_crossing_plans.sh;
 	${XL_IDP_ROOT_DATACORE}/bash/terminals_plans_orlovka_manp.sh;
 	${XL_IDP_ROOT_DATACORE}/bash/terminals_plans_p1-p4.sh;
+	${XL_IDP_ROOT_DATACORE}/bash/sales_plan_pivot_table.sh;
 	sleep 1;
 done
